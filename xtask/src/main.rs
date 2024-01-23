caff8676939afd7ef1daefabb1681c5743c32340
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2024-01-23 12:40:25 +0900

    Update xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,8 +1,7 @@
-use std::env;
 use std::path::PathBuf;
 
-use clap::{Args, CommandFactory, Parser, Subcommand, ValueEnum};
-use clap_complete::{generate_to, shells};
+use clap::{Args, Command, CommandFactory, Parser, Subcommand};
+use clap_complete::{generate, Generator, Shell};
 
 #[derive(Parser)]
 struct Tasks {
@@ -12,54 +11,43 @@ struct Tasks {
 
 #[derive(Subcommand)]
 enum Task {
-    /// Print man
+    /// Save man pages to a directory
     Man(ManArgs),
-    /// Generate completion files. Dry run by default. Add `--install` option to perform actual installation
+    /// Print shell completion
     Complete(CompleteArgs),
 }
 
 #[derive(Debug, Args)]
 struct ManArgs {
-    /// Output directory
-    output: Option<PathBuf>,
+    /// Output directory. e.g. `$MANPATH/man1`
+    output: PathBuf,
 }
 
 #[derive(Debug, Args)]
 struct CompleteArgs {
-    /// Shell
-    shell: Shell,
-    /// Override installation installation directory
-    output: Option<PathBuf>,
-    /// Binary name
-    #[clap(long, default_value = "lmrs")]
-    name: String,
-    /// Execute installation
-    #[clap(long, action)]
-    install: bool,
+    /// Shell to generate completion for
+    generator: Shell,
 }
 
-#[derive(ValueEnum, Debug, Copy, Clone)]
-enum Shell {
-    Bash,
-    Fish,
-    Zsh,
+fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
+    generate(gen, cmd, cmd.get_name().to_string(), &mut std::io::stdout());
 }
 
 fn main() {
     let task = Tasks::parse();
-    let mut cmd = lmrs::cli::Cli::command();
 
     match task.task {
         Task::Man(args) => {
-            let outdir = if let Some(outdir) = args.output.as_ref() {
-                if !outdir.exists() {
-                    std::fs::create_dir_all(outdir).unwrap();
-                }
-                outdir
-            } else {
-                panic!("Default output dierctory is not implemented. Specify OUTPUT");
-            };
+            let outdir = args.output;
+            if outdir.is_file() {
+                panic!("output must be a directory");
+            }
+            if !outdir.exists() {
+                println!("Creating output directory: {:?}", outdir);
+                std::fs::create_dir_all(&outdir).unwrap();
+            }
 
+            let cmd = lmrs::cli::Cli::command();
             let cmd_name: String = cmd.get_name().into();
             let version: String = cmd.get_version().unwrap().into();
             let ext = ".1";
@@ -76,34 +64,9 @@ fn main() {
             let mut file = std::fs::File::create(outname).unwrap();
             clap_mangen::Man::new(cmd).render(&mut file).unwrap();
         }
-        Task::Complete(args) => match args.shell {
-            Shell::Bash => {
-                panic!("Not implemented for bash.");
-                // generate_to(Bash, &mut cmd, &args.name, &args.output).unwrap();
-            }
-            Shell::Fish => {
-                if args.output.is_some() {
-                    panic!("<OUTPUT> argument is invalid for fish shell.");
-                }
-                let config_dir = env::var("XDG_CONFIG_HOME")
-                    .map(PathBuf::from)
-                    .unwrap_or_else(|_| {
-                        home::home_dir()
-                            .map(|d| d.join(".config"))
-                            .expect("Failed to get home directory.")
-                    });
-                let completion_dir = config_dir.join("fish/completions");
-                if args.install {
-                    println!("Installing in {:?}", completion_dir);
-                    generate_to(shells::Fish, &mut cmd, &args.name, completion_dir).unwrap();
-                } else {
-                    println!("Dryrun: install in {:?}", completion_dir);
-                }
-            }
-            Shell::Zsh => {
-                panic!("Not implemented for zsh.");
-                // generate_to(Zsh, &mut cmd, &args.name, &args.output).unwrap();
-            }
-        },
+        Task::Complete(args) => {
+            let mut cmd = lmrs::cli::Cli::command();
+            print_completions(args.generator, &mut cmd);
+        }
     }
 }
